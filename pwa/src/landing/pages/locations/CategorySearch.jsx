542a319b605931bc750697ba62028d07e784b2a8
Aaron Reyes Ruiz <reruaarr@gmail.com>
2025-10-22 04:25:45 -0600

    Feature locations (#27) (#28)
    * Locations section started
    
    * Locations section idea
    
    * Locations section base

--- pwa/src/landing/pages/locations/CategorySearch.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SearchCategory = () => {
-  return (
-    <div>BusquedaCategoria</div>
-  )
-}
-
-export default SearchCategory
--- pwa/src/landing/pages/locations/PointsOfInterest.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InterestPoints = () => {
-  return (
-    <div>PuntosInteres</div>
-  )
-}
-
-export default InterestPoints
