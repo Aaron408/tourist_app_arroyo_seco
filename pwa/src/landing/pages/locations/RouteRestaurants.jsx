25f19b0908791084242276c73920234f7f3bb922
Aaron Reyes Ruiz <reruaarr@gmail.com>
2025-10-22 04:27:43 -0600

    Feature locations (#27) (#28) (#29)
    * Locations section started
    
    * Locations section idea
    
    * Locations section base

--- pwa/src/landing/pages/locations/RouteRestaurants.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RestaurantsRoute = () => {
-  return (
-    <div>RestaurantesRuta</div>
-  )
-}
-
-export default RestaurantsRoute
--- pwa/src/landing/pages/locations/Sketch.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Sketch = () => {
-  return (
-    <div>Croquis</div>
-  )
-}
-
-export default Sketch
